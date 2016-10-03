19bad8d4a04764ac975eba57c69aef5d52da0a66
TheBearCode <alexandre.skipper@gmail.com>
2016-10-02 22:36:05 -0400

    Added android files

---
+++ MyApplication/app/src/main/java/com/example/alex/myapplication/MainMenu.java
@@ -0,0 +1,18 @@
+package com.example.alex.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainMenu);
+    }
+    /*
+    public void settings(View view) {
+
+    }
+    */
+}
