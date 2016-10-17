a5ff14f4fefd61f7d7b9001a6572df6bcee55353
TheBearCode <alexandre.skipper@gmail.com>
2016-10-17 16:46:53 -0400

    Added more activities

--- MyApplication/app/src/main/java/com/example/alex/myapplication/AboutActivity.java
+++ MyApplication/app/src/main/java/com/example/alex/myapplication/AboutActivity.java
@@ -3,12 +3,12 @@ package com.example.alex.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_about);
     }
 
 }
--- MyApplication/app/src/main/java/com/example/alex/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/alex/myapplication/MainActivity.java
@@ -15,11 +15,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         Button button = (Button) findViewById(R.id.button_settings);
-        
+
         button.setOnClickListener(new OnClickListener() {
             public void onClick(View view) {
                 Intent myIntent = new Intent(MainActivity.this,
                         SettingsActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        button = (Button) findViewById(R.id.button_about);
+
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        AboutActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        button = (Button) findViewById(R.id.button_schedule);
+
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        ClassesActivity.class);
                 startActivity(myIntent);
             }
         });
