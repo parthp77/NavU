a79628fcbcfb718a1f3e20b8e0409ba2ee4988b4
Mike Stupich <mike.stupich@gmail.com>
2016-10-22 19:10:34 -0400

    Augmented settings screen, now saves data

--- MyApplication/app/src/main/java/com/example/alex/myapplication/SettingsActivity.java
+++ MyApplication/app/src/main/java/com/example/alex/myapplication/SettingsActivity.java
@@ -1,14 +1,39 @@
 package com.example.alex.myapplication;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.view.MenuItem;
 
-public class SettingsActivity extends AppCompatActivity {
 
+/**
+ * Created by mike on 2016-10-18.
+ */
+
+public class SettingsActivity extends PreferenceActivity {
+    private final static String TAG = "Settings";
+
+    public SettingsActivity(){}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new LocationFragment()).commit();
+
+
+    }
+
+    public static class LocationFragment extends PreferenceFragment {
+
+        private final static String TAG = "LocationFragment";
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+
+        }
     }
 
 }
+
