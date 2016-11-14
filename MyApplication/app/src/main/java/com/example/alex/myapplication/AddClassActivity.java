f03482aad5f43188c2d1cf22daf90052ca33e1eb
Mike Stupich <mike.stupich@gmail.com>
2016-11-14 02:10:22 -0500

    Set up the classes page to allow users to add new classes

--- MyApplication/app/src/main/java/com/example/alex/myapplication/AddClassActivity.java
+++ MyApplication/app/src/main/java/com/example/alex/myapplication/AddClassActivity.java
@@ -1,8 +1,45 @@
 package com.example.alex.myapplication;
 
-/**
- * Created by Ted on 2016-11-06.
- */
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class AddClassActivity {
+
+public class AddClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addclass);
+
+
+        Button b = (Button) findViewById(R.id.AddClassToList);
+        b.setOnClickListener(new OnClickListener(){
+            public void onClick(View view)
+            {
+                saveInfo();
+                Intent myIntent = new Intent(AddClassActivity.this, ClassesActivity.class);
+                startActivity(myIntent);
+            }
+
+        });
+    }
+
+    private void saveInfo(){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor prefEditor = sp.edit();
+        prefEditor.putString("", "Comp 9000");
+        prefEditor.commit();
+
+
+    }
+
+
+
 }
--- MyApplication/app/src/main/java/com/example/alex/myapplication/ClassesActivity.java
+++ MyApplication/app/src/main/java/com/example/alex/myapplication/ClassesActivity.java
@@ -1,21 +1,63 @@
 package com.example.alex.myapplication;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView.OnItemClickListener;
+
+import static com.example.alex.myapplication.R.xml.classes;
 
 public class ClassesActivity extends AppCompatActivity {
-    String[] listEle = {"class1", "class2", "class3"};
+     String[] listEle = new String[5];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classes);
-
+        listEle = fillList();
+        //String [] listEle  = {"C1","C2","C3","C4","C5"};
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, listEle);
 
         ListView listView = (ListView) findViewById(R.id.class_list);
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long l) {
+                Object selectedItem = adapter.getItemAtPosition(position);
+                Intent intent = new Intent(ClassesActivity.this, AddClassActivity.class);
+                startActivity(intent);
+
+            }
+        });
         listView.setAdapter(adapter);
+
+        Button button = (Button) findViewById(R.id.AddClass);
+
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(ClassesActivity.this,
+                        AddClassActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+    private String[] fillList(){
+        //String[] list = new String[5];
+        String[] list  = {"COMP9001","COMP3000","COMP3004","ENG1000","ELEC1001"};
+        SharedPreferences sp = ClassesActivity.this.getSharedPreferences("saved_classes", 0);
+        Log.v("Tag", sp.getString("class1", list[0]));
+        //list[1] = sp.getString("Class2", "temp2");
+
+        return list;
+
     }
 
 }
